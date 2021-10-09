93310ae961e402b4e337b708870aeb74d3ecbd05
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-10-10 01:32:28 +0800

    it's working!

--- main.py
+++ main.py
@@ -4,36 +4,51 @@ from dotenv import load_dotenv
 import os
 import pytz
 from flask_cors import CORS
+from pyairtable import Table
+from flaskext.markdown import Markdown
+
 load_dotenv()
 
+api_key = os.getenv("AIRTABLE_API_KEY")
 path = "" if os.getenv("LOCAL_DEV") else "https://" + os.getenv("DETA_PATH")
 host = ".deta.app" if os.getenv("DETA_SPACE_APP") else "" if os.getenv(
     "LOCAL_DEV") else ".deta.dev"
+updatesTable = Table(api_key, 'appuc2zlKlrEX6OSv', 'Updates')
+feedbackTable = Table(api_key, 'appuc2zlKlrEX6OSv', 'Feedback')
 
 app = Flask(__name__)
+
+Markdown(app)
 
 CORS(app)
 
 utc = pytz.utc
 
-@app.route('/', methods=["GET"])
+@app.route('/', methods=["GET", "POST"])
 def index():
-    r = make_response(render_template('index.html'))
+    submitted = False
+    if request.method == 'POST':
+        feedbackTable.create({'Status': 'To Be Read', "Subject": request.form['subject'], "Message": request.form['message']})
+        submitted = True
+    updates = updatesTable.all(sort=["-Publish Date"])
+    r = make_response(render_template('index.html', updates=updates, submitted=submitted))
     r.headers.set('cache-control', 'no-store')
     return r
 
 @app.route('/updates', methods=["GET"])
 def updates():
-    r = make_response(render_template('updates.html'))
+    updates = updatesTable.all(sort=["-Publish Date"])
+    r = make_response(render_template('updates.html', updates=updates))
     r.headers.set('cache-control', 'no-store')
     return r
 
-@app.route('/post', methods=["GET"])
-def post():
-    r = make_response(render_template('post.html'))
+@app.route('/post/<id>', methods=["GET"])
+def post(id):
+    post = updatesTable.get(id)
+    updates = updatesTable.all(sort=["-Publish Date"])
+    r = make_response(render_template('post.html', post=post, updates=updates))
     r.headers.set('cache-control', 'no-store')
     return r
-
 
 if __name__ == "__main__":
     app.run(debug=True)
